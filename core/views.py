7492412da91c35e0b5bbe6666bd172cc136e7815
Brandon Hernandez <al192880@alumnos.uacj.mx>
2025-08-15 02:30:51 -0600

    Agrego base del proyecto

--- core/views.py
+++ core/views.py
@@ -1,3 +1,7 @@
+# core/views.py
+
 from django.shortcuts import render
 
-# Create your views here.
+def inicio(request):
+    return render(request, 'core/home.html')  # <-- incluyendo subcarpeta
+
