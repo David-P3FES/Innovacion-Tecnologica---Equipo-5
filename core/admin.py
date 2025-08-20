27eb50baefab554f1a563e58fcc5b2044955ddfb
192880 Brandon Hernandez <al192880@alumnos.uacj.mx>
2025-08-19 23:05:44 -0600

    Plantilla de login lista

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,6 @@
+# core/admin.py (o donde tengas el modelo de usuario)
 from django.contrib import admin
+from django.contrib.auth import get_user_model
 
-# Register your models here.
+User = get_user_model()
+
