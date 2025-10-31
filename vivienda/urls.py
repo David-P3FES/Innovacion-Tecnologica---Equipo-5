8225c2e2a003e9d6d58b5b44f7beadb4ec3e51b5
NeidaFranco_214981 <al214981@alumnos.uacj.mx>
2025-10-30 22:13:33 -0600

    merge completo

--- vivienda/urls.py
+++ vivienda/urls.py
@@ -2,18 +2,17 @@
 @file urls.py
 @brief Enrutador principal del proyecto Django "vivienda".
 @details
-Define las rutas globales del proyecto:
-- Panel de administración de Django.
-- Rutas de la aplicación `principal` (inicio y resultados).
-- Rutas de la aplicación `cuentas` (perfil y usuario).
-- Rutas de autenticación social con Allauth.
+ Define las rutas globales del proyecto:
+ - Panel de administración de Django.
+ - Rutas de la aplicación `principal` (inicio y resultados).
+ - Rutas de la aplicación `cuentas` (perfil y usuario).
+ - Rutas de autenticación social con Allauth.
 """
 
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-
 from django.urls import include, path
 
 urlpatterns = [
@@ -21,26 +20,17 @@ urlpatterns = [
         'admin/',
         admin.site.urls
     ),
+
     path(
         '',
         include('principal.urls')
     ),
+
     path(
         'cuenta/',
         include('cuentas.urls')
     ),
-
-    path(
-        'cuenta/',
-        include('allauth.urls')
-    ),
-    path("publicaciones/", include("publicaciones.urls")),
-
-    path(
-        '',
-        include('principal.urls')
-    ),
-
+    
     path(
         'cuenta/',
         include('allauth.urls')
@@ -49,6 +39,9 @@ urlpatterns = [
     path("publicaciones/", include("publicaciones.urls")),
 
     path("billing/", include("billing.urls")),
+
+    path('cuentas/', include('cuentas.urls', namespace='cuentas')),
+
 
 ]
 
