efd45f9893bf5d297217a173434a0f9e231bdbde
BrandonGus <al192880@alumnos.uacj.mx>
2025-11-13 15:30:42 -0700

    Add AWS EB domain to ALLOWED_HOSTS

--- vivienda/settings.py
+++ vivienda/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # ==============================
 SECRET_KEY = 'django-insecure-=#+e_+@am2c6-+&44b1cauo1ux@d0u*s&39@q-*xqa1c@6+w^7'
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 # ==============================
 # Aplicaciones instaladas
